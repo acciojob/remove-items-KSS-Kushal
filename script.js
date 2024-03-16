0b0c6a950a502617505f3e2a22ecad5c0d596323
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-16 22:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const handleClick = () => {
+	const selectHTML = document.getElementById("colorSelect");
+	const color = selectHTML.value;
+	const colorList = selectHTML.options;
+	for (let i = 0; i < colorList.length; i++) {
+		if (colorList[i].innerHTML===color) {
+			selectHTML.removeChild(colorList[i]);
+		}
+	}
+}
